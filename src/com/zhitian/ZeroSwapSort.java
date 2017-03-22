ce903a48d361a9c79c9b5530fc8edf9a714129a3
SkyeBeFreeman <928016560@qq.com>
2017-03-23 00:22:41 +0800

    update zero swap sort

--- src/com/zhitian/ZeroSwapSort.java
+++ src/com/zhitian/ZeroSwapSort.java
@@ -5,7 +5,7 @@ package com.zhitian;
  */
 public class ZeroSwapSort implements MySort {
 
-    public int[] a = {3,6,5,8,7,1,4,9,0,2};
+    public int[] a = {3, 6, 5, 8, 7, 1, 4, 9, 0, 2};
 
 
     @Override
@@ -17,21 +17,18 @@ public class ZeroSwapSort implements MySort {
     /**
      * 交换数组里n和0的位置
      *
-     * @param array
-     *            数组
-     * @param len
-     *            数组长度
-     * @param n
-     *            和0交换的数
+     * @param array 数组
+     * @param len   数组长度
+     * @param n     和0交换的数
      */
     public void swapWithZero(int[] array, int len, int n) {
         int zIndex = -1;
         int nIndex = -1;
-        for(int i = 0; i < len; ++i){
-            if(array[i] == 0){
+        for (int i = 0; i < len; ++i) {
+            if (array[i] == 0) {
                 zIndex = i;
             }
-            if(array[i] == n){
+            if (array[i] == n) {
                 nIndex = i;
             }
         }
@@ -44,24 +41,16 @@ public class ZeroSwapSort implements MySort {
     /**
      * 通过调用swapWithZero方法来排
      *
-     * @param array
-     *            存储有[0,n)的数组
-     * @param len
-     *            数组长度
+     * @param array 存储有[0,n)的数组
+     * @param len   数组长度
      */
     public void sort(int[] array, int len) {
-        if(len <= 1){
+        if (len <= 1) {
             return;
         }
-        for(int i = len - 1; i > 0; --i){
+        for (int i = 0; i < len; i++) {
             swapWithZero(array, len, array[i]);
-            int curMax = array[i];
-            for(int j = i; j >= 0; --j){
-                if(array[j] > curMax){
-                    curMax = array[j];
-                }
-            }
-            swapWithZero(array, len, curMax);
+            swapWithZero(array, len, i);
         }
     }
 
